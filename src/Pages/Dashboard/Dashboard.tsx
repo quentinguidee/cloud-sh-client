2f87e302db9878ce30eb57976f0c949e232b8c85
Quentin Guidée <quentin.guidee@gmail.com>
2022-04-13 18:11:12 +0200

    Content layout
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/Pages/Dashboard/Dashboard.tsx
+++ src/Pages/Dashboard/Dashboard.tsx
@@ -1,19 +1,24 @@
-import React, { Fragment } from "react";
+import React from "react";
 import NavigationBar from "Layouts/NavigationBar/NavigationBar";
 import Drive from "Pages/Drive/Drive";
 import { Route, Routes } from "react-router-dom";
+import Layout from "Components/Layout/Layout";
+
+import styles from "./Dashboard.module.sass";
 
 function Dashboard() {
     return (
-        <Fragment>
-            <Routes>
-                <Route path="drive" element={<Drive />} />
-            </Routes>
-
+        <Layout horizontal stretch>
             <div>
                 <NavigationBar />
             </div>
-        </Fragment>
+
+            <div className={styles.content}>
+                <Routes>
+                    <Route path="drive" element={<Drive />} />
+                </Routes>
+            </div>
+        </Layout>
     );
 }
 
