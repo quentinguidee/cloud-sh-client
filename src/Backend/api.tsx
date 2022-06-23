bf236afdd5c985b3703e5222af37c6387ec0710d
Quentin Guidée <quentin.guidee@gmail.com>
2022-06-23 18:09:26 +0200

    Support GitHub authorization
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/Backend/api.tsx
+++ src/Backend/api.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 // TODO: .env
 const API_URL = "http://localhost:8080";
 
-async function get(route: string) {
+export async function get(route: string) {
     try {
         const res = await axios.get(`${API_URL}${route}`);
         return [res, null];
@@ -12,4 +12,11 @@ async function get(route: string) {
     }
 }
 
-export { get };
+export async function post(route: string, data: object) {
+    try {
+        const res = await axios.post(`${API_URL}${route}`, data);
+        return [res, null];
+    } catch (err) {
+        return [null, err];
+    }
+}
