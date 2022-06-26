50b4a02f90cefa42b8f6fcd8f46502b575114e79
Quentin Guidée <quentin.guidee@gmail.com>
2022-06-26 09:52:32 +0200

    Webpack: Fix subroutes not working
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "out"),
         filename: "bundle.[hash].js",
+        publicPath: "/",
     },
     module: {
         rules: [
