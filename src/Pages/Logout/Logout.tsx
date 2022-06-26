c9d1ac7d03206cd9e07a887732d4c7e1b8a20d4a
Hector <54723548+Syycorax@users.noreply.github.com>
2022-06-26 18:22:25 +0200

    Add style to logout (#8)
    * Added style to logout
    
    * removed useless newline
    
    * Update src/Pages/Logout/Logout.tsx
    
    Co-authored-by: Quentin Guidée <quentin.guidee@gmail.com>
    
    * Update src/Pages/Logout/Logout.tsx
    
    Co-authored-by: Quentin Guidée <quentin.guidee@gmail.com>
    
    Co-authored-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/Pages/Logout/Logout.tsx
+++ src/Pages/Logout/Logout.tsx
@@ -3,6 +3,10 @@ import { useNavigate } from "react-router-dom";
 import { useSession } from "Store/Hooks/useSession";
 import axios from "axios";
 import { route } from "Backend/api";
+import Box from "Components/Box/Box";
+import styles from "./Logout.module.sass";
+import Layout from "Components/Layout/Layout";
+import { Subtitle } from "Components/Title/Title";
 
 function Logout() {
     const navigate = useNavigate();
@@ -20,7 +24,15 @@ function Logout() {
         logout().then(undefined);
     }, []);
 
-    return <div>Disconnecting...</div>;
+    return(
+        <div className={styles.content}>
+            <Layout vertical className={styles.logout}>
+                <Box>
+                    <Subtitle>Disconnecting...</Subtitle>
+                </Box>
+            </Layout>
+        </div>
+    );
 }
 
 export default Logout;
