7b2967bc16f6665a1553aecc8f02dcfe6ff12138
Quentin Guidée <quentin.guidee@gmail.com>
2022-06-26 12:09:19 +0200

    Popover: Dismiss with ESC
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/Components/Popover/Popover.tsx
+++ src/Components/Popover/Popover.tsx
@@ -21,8 +21,17 @@ function Popover(props: Props) {
         if (onClose) onClose();
     };
 
+    const onKeyDown = (e) => {
+        if (e.key === "Escape") {
+            dismiss();
+        }
+    };
+
     useEffect(() => {
         setShow(props.show);
+        props.show
+            ? document.addEventListener("keydown", onKeyDown)
+            : document.removeEventListener("keydown", onKeyDown);
     }, [props.show]);
 
     return (
