f517db4e0aad3295fcf53cea4d275884c0ae27ca
Syycorax <54723548+Syycorax@users.noreply.github.com>
2022-06-26 18:16:35 +0200

    Added style to logout

--- src/Pages/Logout/Logout.tsx
+++ src/Pages/Logout/Logout.tsx
@@ -3,6 +3,10 @@ import { useNavigate } from "react-router-dom";
 import { useSession } from "Store/Hooks/useSession";
 import axios from "axios";
 import { route } from "Backend/api";
+import Box from "Components/Box/Box";
+import styles from "./Logout.module.sass";
+import Layout from "Components/Layout/Layout";
+import { Subtitle } from "Components/Title/Title";
 
 function Logout() {
     const navigate = useNavigate();
@@ -14,13 +18,22 @@ function Logout() {
             .post(route("/auth/logout"), { ...session })
             .then(() => navigate("/login"))
             .catch(console.error);
+        
     };
 
     useEffect(() => {
         logout().then(undefined);
     }, []);
 
-    return <div>Disconnecting...</div>;
+    return(
+        <div className={styles.content}>
+            <Layout vertical className={styles.logout}>
+                    <Box>
+                            <Subtitle>Disconnecting ...</Subtitle>
+                    </Box>
+            </Layout>
+        </div>
+    );
 }
 
 export default Logout;
