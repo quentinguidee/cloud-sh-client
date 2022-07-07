45cd9aeef222967c2103547f308f887d2628cbd5
Quentin Guidée <quentin.guidee@gmail.com>
2022-07-07 17:16:43 +0200

    Fix layout custom styles
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/Components/Layout/Layout.tsx
+++ src/Components/Layout/Layout.tsx
@@ -60,7 +60,7 @@ function Layout(props: Props) {
                 [styles.layoutMaximize]: maximize,
                 [className]: true,
             })}
-            style={{ gap, ...styles }}
+            style={{ gap, ...style }}
         />
     );
 }
