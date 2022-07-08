068ded974850a3d2213d961b7df849f2e6440b79
Quentin Guidée <quentin.guidee@gmail.com>
2022-07-08 21:40:45 +0200

    Preview images
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/Components/NodePreview/NodePreview.tsx
+++ src/Components/NodePreview/NodePreview.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Overlay from "Components/Overlay/Overlay";
 import styles from "./NodePreview.module.sass";
 import { Node } from "Models/Node";
 import classNames from "classnames";
+import axios from "axios";
+import { api, route } from "Backend/api";
+import { useSession } from "Store/Hooks/useSession";
+import Layout from "Components/Layout/Layout";
+import Spacer from "Components/Spacer/Spacer";
+import Close from "Components/Close/Close";
+import { Caption } from "Components/Text/Text";
+import { Subtitle } from "Components/Title/Title";
 
 type Props = {
     node?: Node;
@@ -10,7 +18,41 @@ type Props = {
 };
 
 function NodePreview(props: Props) {
-    const { node, onClose } = props;
+    const { node } = props;
+
+    const session = useSession();
+
+    const [image, setImage] = useState(undefined);
+
+    const downloadNode = (node: Node) => {
+        axios({
+            method: "GET",
+            url: route("/storage/download"),
+            params: {
+                node_uuid: node.uuid,
+            },
+            headers: {
+                Authorization: session.token,
+            },
+            responseType: "blob",
+        })
+            .then((res) => {
+                setImage(URL.createObjectURL(res.data));
+            })
+            .catch(api.error);
+    };
+
+    useEffect(() => {
+        // TODO: Change type to mime
+        if (node?.type === "image") {
+            downloadNode(node);
+        }
+    }, [node]);
+
+    const onClose = () => {
+        setImage(undefined);
+        if (props.onClose) props.onClose();
+    };
 
     return (
         <React.Fragment>
@@ -20,7 +62,26 @@ function NodePreview(props: Props) {
                     [styles.window]: true,
                     [styles.windowShow]: node !== undefined,
                 })}
-            ></div>
+            >
+                <Layout horizontal center className={styles.titleBar}>
+                    <Layout vertical left gap={2}>
+                        <Subtitle>Preview</Subtitle>
+                        <Caption>{node?.name}</Caption>
+                    </Layout>
+                    <Spacer />
+                    <Spacer width={24} />
+                    <Close onClick={onClose} />
+                </Layout>
+                <div className={styles.contentWrapper}>
+                    {image && (
+                        <img
+                            alt={node.name}
+                            src={image}
+                            className={styles.content}
+                        />
+                    )}
+                </div>
+            </div>
         </React.Fragment>
     );
 }
