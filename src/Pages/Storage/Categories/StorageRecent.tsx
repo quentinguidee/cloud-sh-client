bd3419b72860fa72f5914162bbd21fcb6aed788a
Quentin Guidée <quentin.guidee@gmail.com>
2022-07-20 16:32:44 +0200

    Get recent files
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/Pages/Storage/Categories/StorageRecent.tsx
+++ src/Pages/Storage/Categories/StorageRecent.tsx
@@ -1,14 +1,38 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import FileExplorer from "Layouts/FileExplorer/FileExplorer";
 import TitleBar from "Layouts/TitleBar/TitleBar";
+import axios from "axios";
+import { api, route } from "Backend/api";
+import { useSession } from "Store/Hooks/useSession";
+import { Node } from "Models/Node";
 
 function StorageRecent() {
-    const reload = () => {};
+    const session = useSession();
+
+    const [nodes, setNodes] = useState<Node[]>([]);
+
+    useEffect(() => {
+        reload();
+    }, []);
+
+    const reload = () => {
+        axios({
+            url: route("/storage/recent"),
+            headers: {
+                Authorization: session.token,
+            },
+        })
+            .then((res) => {
+                console.table(res.data.nodes);
+                setNodes(res.data.nodes);
+            })
+            .catch(api.error);
+    };
 
     return (
         <Fragment>
             <TitleBar title="Recent" />
-            <FileExplorer onReload={reload} />
+            <FileExplorer nodes={nodes} onReload={reload} />
         </Fragment>
     );
 }
