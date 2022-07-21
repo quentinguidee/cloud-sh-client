8b977fc7ed18dbcd40f241b82d0509983ef8869f
Quentin Guidée <quentin.guidee@gmail.com>
2022-07-21 15:34:08 +0200

    Show bucket size and limit
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/Pages/Storage/StorageNavBar.tsx
+++ src/Pages/Storage/StorageNavBar.tsx
@@ -1,8 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import styles from "./StorageNavBar.module.sass";
+
 import NavBar from "Components/NavBar/NavBar";
 import NavBarItem from "Components/NavBarItem/NavBarItem";
-
 import Layout from "Components/Layout/Layout";
+import ProgressBar from "Components/ProgressBar/ProgressBar";
+import { Caption } from "Components/Text/Text";
+import axios from "axios";
+import { api, route } from "Backend/api";
+import { useSession } from "Store/Hooks/useSession";
+import prettyBytes from "pretty-bytes";
+
+function StorageSize() {
+    const session = useSession();
+
+    const [bucketSize, setBucketSize] = useState<number>();
+    const [bucketLimit, setBucketLimit] = useState<number>();
+
+    useEffect(() => {
+        axios({
+            method: "GET",
+            url: route("/storage/bucket"),
+            headers: {
+                Authorization: session.token,
+            },
+        })
+            .then((res) => {
+                const { size, max_size } = res.data;
+                setBucketSize(size);
+                setBucketLimit(max_size);
+            })
+            .catch(api.error);
+    }, []);
+
+    if (!bucketSize) return;
+
+    if (!bucketLimit) {
+        return (
+            <Layout vertical gap={6} className={styles.storageProgress}>
+                <Caption>Stored: {prettyBytes(bucketSize)}</Caption>
+            </Layout>
+        );
+    }
+
+    return (
+        <Layout vertical gap={6} className={styles.storageProgress}>
+            <Caption>
+                {prettyBytes(bucketSize)}/{prettyBytes(bucketLimit)}
+            </Caption>
+            <ProgressBar percentage={(bucketSize / bucketLimit) * 100} />
+        </Layout>
+    );
+}
 
 function StorageNavBar() {
     return (
@@ -17,6 +67,7 @@ function StorageNavBar() {
                 <NavBarItem to="/storage/bin" icon="delete">
                     Bin
                 </NavBarItem>
+                <StorageSize />
             </Layout>
         </NavBar>
     );
