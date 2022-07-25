1a87ed0a3ed88410f2a9e2123f1d20c504337f48
Quentin Guidée <quentin.guidee@gmail.com>
2022-07-25 13:28:45 +0200

    Empty bin button
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/Pages/Storage/Categories/StorageBin.tsx
+++ src/Pages/Storage/Categories/StorageBin.tsx
@@ -6,6 +6,10 @@ import { api, route } from "Backend/api";
 import TitleBar from "Layouts/TitleBar/TitleBar";
 import FileExplorer from "Layouts/FileExplorer/FileExplorer";
 import { Bucket } from "Models/Bucket";
+import Toolbar from "Components/Toolbar/Toolbar";
+import ToolbarItem from "Components/ToolbarItem/ToolbarItem";
+import Spacer from "Components/Spacer/Spacer";
+import Layout from "Components/Layout/Layout";
 
 type Props = {
     bucket: Bucket;
@@ -36,9 +40,31 @@ function StorageBin(props: Props) {
             .catch(api.error);
     };
 
+    const emptyBin = () => {
+        axios({
+            method: "DELETE",
+            url: route(`/storage/${bucket.uuid}/bin`),
+            headers: {
+                Authorization: session.token,
+            },
+        })
+            .then(() => reload())
+            .catch(api.error);
+    };
+
     return (
         <Fragment>
             <TitleBar title="Bin" />
+            <Layout vertical gap={20}>
+                <Toolbar>
+                    <ToolbarItem
+                        symbol="delete_forever"
+                        text="Empty bin"
+                        onClick={emptyBin}
+                    />
+                </Toolbar>
+                <Spacer />
+            </Layout>
             <FileExplorer
                 bucket={bucket}
                 nodes={nodes}
