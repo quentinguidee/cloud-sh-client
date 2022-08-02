5b4045497a9f66ee6964adf05c7609eb06ce00a0
Quentin Guidée <quentin.guidee@gmail.com>
2022-08-02 19:45:38 +0200

    OAuth configuration
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/Components/Input/Input.tsx
+++ src/Components/Input/Input.tsx
@@ -2,23 +2,33 @@ import React, { Ref } from "react";
 
 import styles from "./Input.module.sass";
 import classNames from "classnames";
+import Layout from "Components/Layout/Layout";
 
 type Props = React.HTMLProps<HTMLInputElement> & {
     small?: boolean;
+    label?: string;
 };
 
 function Input(props: Props, ref: Ref<HTMLInputElement>) {
-    const { className, small, ...others } = props;
+    const { className, small, label, name, ...others } = props;
     return (
-        <input
-            ref={ref}
-            className={classNames({
-                [styles.input]: true,
-                [styles.inputSmall]: small,
-                [className]: true,
-            })}
-            {...others}
-        />
+        <Layout vertical stretch gap={8}>
+            {label && (
+                <label className={styles.label} htmlFor={name}>
+                    {label}
+                </label>
+            )}
+            <input
+                ref={ref}
+                name={name}
+                className={classNames({
+                    [styles.input]: true,
+                    [styles.inputSmall]: small,
+                    [className]: true,
+                })}
+                {...others}
+            />
+        </Layout>
     );
 }
 
--- src/Pages/ServerConfig/ServerConfig.tsx
+++ src/Pages/ServerConfig/ServerConfig.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./ServerConfig.module.sass";
 import { Subtitle } from "Components/Title/Title";
 import Input from "Components/Input/Input";
@@ -10,13 +10,36 @@ import axios from "axios";
 import { api, route } from "Backend/api";
 import { useNavigate } from "react-router-dom";
 
-function DatabaseConfig() {
+type StepProps = {
+    onDone: () => void;
+};
+
+function DatabaseConfig(props: StepProps) {
+    const [loading, setLoading] = useState<boolean>(true);
+
     const [host, setHost] = useState<string>();
     const [name, setName] = useState<string>();
     const [user, setUser] = useState<string>();
     const [password, setPassword] = useState<string>();
 
-    const navigate = useNavigate();
+    const load = () => {
+        axios({
+            method: "GET",
+            url: route("/config/database"),
+        })
+            .then((res) => {
+                if (res.data.already_done) {
+                    props.onDone();
+                } else {
+                    setLoading(false);
+                }
+            })
+            .catch(api.error);
+    };
+
+    useEffect(() => load(), []);
+
+    if (loading) return null;
 
     const save = () => {
         axios({
@@ -24,7 +47,7 @@ function DatabaseConfig() {
             url: route("/config/database"),
             params: { host, name, user, password },
         })
-            .then(() => navigate("/login"))
+            .then(() => props.onDone())
             .catch(api.error);
     };
 
@@ -35,37 +58,47 @@ function DatabaseConfig() {
 
     return (
         <div className={styles.content}>
-            <Layout vertical left gap={24}>
-                <Subtitle>Database setup</Subtitle>
-                <Layout right vertical gap={20}>
-                    <Layout stretch vertical gap={12} className={styles.fields}>
+            <Layout vertical left gap={32}>
+                <Subtitle style={{ marginLeft: 12 }}>
+                    Authentication setup
+                </Subtitle>
+                <Layout right vertical gap={32}>
+                    <Layout stretch vertical gap={20} className={styles.fields}>
                         <Input
-                            placeholder="URL"
+                            label="URL"
+                            name="url"
+                            placeholder="localhost"
                             type="url"
                             value={host}
                             onChange={onHostChange}
                         />
                         <Input
-                            placeholder="Database"
+                            label="Name"
+                            name="name"
+                            placeholder="cloudsh"
                             type="text"
                             value={name}
                             onChange={onNameChange}
                         />
                         <Input
-                            placeholder="Username"
+                            label="Username"
+                            name="username"
+                            placeholder="jean.dupont"
                             type="text"
                             value={user}
                             onChange={onUserChange}
                         />
                         <Input
-                            placeholder="Password"
+                            label="Password"
+                            name="password"
+                            placeholder="***"
                             type="password"
                             value={password}
                             onChange={onPasswordChange}
                         />
                     </Layout>
                     <Button onClick={save}>
-                        <Text>Try to connect</Text>
+                        <Text>Connect</Text>
                         <Symbol symbol="network_check" />
                     </Button>
                 </Layout>
@@ -74,8 +107,149 @@ function DatabaseConfig() {
     );
 }
 
+function OAuthConfig(props: StepProps) {
+    const [name, setName] = useState<string>();
+    const [color, setColor] = useState<string>();
+    const [clientID, setClientID] = useState<string>();
+    const [clientSecret, setClientSecret] = useState<string>();
+    const [authorizeURL, setAuthorizeURL] = useState<string>();
+    const [accessTokenURL, setAccessTokenURL] = useState<string>();
+    const [redirectURL, setRedirectURL] = useState<string>();
+
+    const save = () => {
+        axios({
+            method: "POST",
+            url: route("/config/oauth"),
+            params: {
+                name,
+                color,
+                client_id: clientID,
+                client_secret: clientSecret,
+                authorize_url: authorizeURL,
+                access_token_url: accessTokenURL,
+                redirect_url: redirectURL,
+            },
+        })
+            .then(() => props.onDone())
+            .catch(api.error);
+    };
+
+    const onNameChange = (e) => {
+        setName(e.target.value);
+    };
+    const onColorChange = (e) => {
+        setColor(e.target.value);
+    };
+    const onClientIDChange = (e) => {
+        setClientID(e.target.value);
+    };
+    const onClientSecretChange = (e) => {
+        setClientSecret(e.target.value);
+    };
+    const onAuthorizeURLChange = (e) => {
+        setAuthorizeURL(e.target.value);
+    };
+    const onAccessTokenURLChange = (e) => {
+        setAccessTokenURL(e.target.value);
+    };
+    const onRedirectURLChange = (e) => {
+        setRedirectURL(e.target.value);
+    };
+
+    return (
+        <div className={styles.content}>
+            <Layout vertical left gap={32}>
+                <Subtitle style={{ marginLeft: 12 }}>Database setup</Subtitle>
+                <Layout right vertical gap={32}>
+                    <Layout stretch vertical gap={20} className={styles.fields}>
+                        <Input
+                            label="Provider"
+                            name="name"
+                            placeholder="GitHub"
+                            type="text"
+                            value={name}
+                            onChange={onNameChange}
+                        />
+                        <Input
+                            label="Color"
+                            name="color"
+                            type="color"
+                            value={color}
+                            onChange={onColorChange}
+                        />
+                        <Input
+                            label="Client ID"
+                            name="client_id"
+                            type="text"
+                            value={clientID}
+                            onChange={onClientIDChange}
+                        />
+                        <Input
+                            label="Client Secret"
+                            name="client_secret"
+                            placeholder="***"
+                            type="password"
+                            value={clientSecret}
+                            onChange={onClientSecretChange}
+                        />
+                        <Input
+                            label="Authorize URL"
+                            name="authorize_url"
+                            placeholder="https://..."
+                            type="url"
+                            value={authorizeURL}
+                            onChange={onAuthorizeURLChange}
+                        />
+                        <Input
+                            label="Access Token URL"
+                            name="access_token_url"
+                            placeholder="https://..."
+                            type="url"
+                            value={accessTokenURL}
+                            onChange={onAccessTokenURLChange}
+                        />
+                        <Input
+                            label="Redirect URL"
+                            name="redirect_url"
+                            placeholder="https://..."
+                            type="url"
+                            value={redirectURL}
+                            onChange={onRedirectURLChange}
+                        />
+                    </Layout>
+                    <Button onClick={save}>
+                        <Text>OK</Text>
+                        <Symbol symbol="done" />
+                    </Button>
+                </Layout>
+            </Layout>
+        </div>
+    );
+}
+
+type Step = "database" | "oauth";
+
 function ServerConfig() {
-    return <DatabaseConfig />;
+    const [step, setStep] = useState<Step>("database");
+
+    const navigate = useNavigate();
+
+    const next = (): Step => {
+        if (step === "database") {
+            setStep("oauth");
+            return;
+        }
+        navigate("/login");
+    };
+
+    switch (step) {
+        case "database":
+            return <DatabaseConfig onDone={next} />;
+        case "oauth":
+            return <OAuthConfig onDone={next} />;
+    }
+
+    return null;
 }
 
 export default ServerConfig;
