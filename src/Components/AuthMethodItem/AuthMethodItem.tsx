aba658406f2c1ec9e697d8cc3e4ca0f94d09015a
Quentin Guidée <quentin.guidee@gmail.com>
2022-08-02 14:42:28 +0200

    Add more info to the AdminAuth category
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/Components/AuthMethodItem/AuthMethodItem.tsx
+++ src/Components/AuthMethodItem/AuthMethodItem.tsx
@@ -4,6 +4,7 @@ import { Caption, Text } from "Components/Text/Text";
 import React from "react";
 import { AuthMethod } from "Models/AuthMethod";
 import Code from "Components/Code/Code";
+import Spacer from "Components/Spacer/Spacer";
 
 type Props = {
     method: AuthMethod;
@@ -14,8 +15,21 @@ function AuthMethodItem(props: Props) {
     return (
         <Layout vertical top left gap={8} className={styles.item}>
             <Text>{method.display_name}</Text>
+            <Spacer />
             <Caption>
                 Client ID: <Code>{method.client_id}</Code>
+            </Caption>
+            <Caption>
+                Client Secret: <Code>{method.client_secret}</Code>
+            </Caption>
+            <Caption>
+                Authorize URL: <Code>{method.authorize_url}</Code>
+            </Caption>
+            <Caption>
+                Access Token URL: <Code>{method.access_token_url}</Code>
+            </Caption>
+            <Caption>
+                Redirect URL: <Code>{method.redirect_url}</Code>
             </Caption>
         </Layout>
     );
--- src/Models/AuthMethod.tsx
+++ src/Models/AuthMethod.tsx
@@ -2,6 +2,9 @@ export type AuthMethod = {
     name: string;
     display_name: string;
     color: string;
-    client_id?: string;
+    client_id: string;
     client_secret?: string;
+    authorize_url?: string;
+    access_token_url?: string;
+    redirect_url?: string;
 };
